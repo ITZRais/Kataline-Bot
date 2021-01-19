f68b2c11eedc20f476e50d65117fe4c2ac02ae2f
ITZRais <75348541+ITZRais@users.noreply.github.com>
2021-01-19 11:24:48 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 var config = require('./config.json');
 var Discord = require('discord.js');
 var client = new Discord.Client();
-
+const kataline = new Client({
+	clientSettings: {
+	    	presence: {
+				activity: {
+						name: 'Kataline'
+					, type:'WATCHING'
+					, url: 'github.com/ITZRais/Kataline-Bot'
+				}
+			}
+        
 client.on('ready', async() => {
-    console.log('Kataline is online!');
+    console.log('Iam Online BITCHES');
 });
 
 client.on('message', async(msg) => {
@@ -17,7 +26,13 @@ client.on('message', async(msg) => {
     var cmd = args.shift().slice(prefix.length).toLowerCase();
 
     try {
-        var file = require(`./commands/${cmd}.js`);
+        var file = require(`./commands/Editable/${cmd}.js`);
+        file.run(client, msg, args);
+    } catch(err) {
+        console.warn(err);
+    }
+    try {
+        var file = require(`./commands/Don't Edit/${cmd}.js`);
         file.run(client, msg, args);
     } catch(err) {
         console.warn(err);
